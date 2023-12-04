cff71adbba565fa93d688e612786bfe7afbbd271
qquareunm <dias2003krut@gmail.com>
2023-12-05 03:25:25 +0600

    Изменения внутренней структуры окончены

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,46 +1,35 @@
-
 import React, {Component} from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import Row from "../row/row";
 import ErrorIndicator from "../error-indicator";
-
 import './app.css';
-
 import PeoplePage from "../people-page";
 import ItemList from "../item-list";
-import ItemDetails, {Record} from "../item-details/item-details.js";
+import ItemDetails, {Record} from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
 import ErrorBoundry from "../error-boundry";
-
-
-
+import {PersonList, PlanetList, StarshipList, PersonDetails, StarshipDetails, PlanetDetails} from "../sw-components";
 export default class App extends Component{
     swapiService = new SwapiService();
-
     state = {
         showRandomPlanet: true
     };
-
-
     toggleRandomPlanet = () => {
         this.setState((state)=> {
             return { showRandomPlanet: !state.showRandomPlanet }
         })
     };
-
     render() {
         const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-
-        const { getPerson,
+        /*const { getPerson,
             getStarship,
             getPersonImage,
             getStarshipImage,
             getAllPeople,
-            getAllPlanets } = this.swapiService;
-
-        const personDetails = <ItemDetails
+            getAllPlanets } = this.swapiService;*/
+        /*const personDetails = <ItemDetails
             itemId={11}
             getData={getPerson}
             getImageUrl={getPersonImage}
@@ -56,8 +45,7 @@ export default class App extends Component{
             <Record field="model" label="Model"/>
             <Record field="length" label="Length"/>
             <Record field="costInCredits" label="Cost"/>
-        </ItemDetails>;
-
+        </ItemDetails>;*/
         return (
             <ErrorBoundry>
                 <div className='container-fluid'>
@@ -66,20 +54,19 @@ export default class App extends Component{
                             <Header/>
                         </div>
                     </div>
-                  
-                    <ItemList
-                        getData={getAllPeople}
-                        onItemSelected={() => {}}>
+                    <PersonDetails itemId={11} />
+                    <StarshipDetails itemId={5} />
+                    <PlanetDetails itemId={9} />
 
-                        { ({name}) => <span>{name}</span> }
-                    </ItemList>
+                    <PersonList />
+                    <StarshipList />
+                    <PlanetList />
 
-                    <ItemList
+                    {/*<ItemList
                         getData={getAllPlanets}
                         onItemSelected={() => {}}>
-
                         { ({name}) => <span>{name}</span> }
-                    </ItemList>
+                    </ItemList>*/}
                 </div>
             </ErrorBoundry>
         );
